1406ad0d1cafe01dcd2059ed99fb6c715afea9b0
John Fazekas <john.a.fazekas@gmail.com>
2019-10-06 01:36:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,20 @@
- def display_board(board)
+def display_board(board)
    puts " #{board[0]} | #{board[1]} | #{board[2]} "
    puts "-----------"
    puts " #{board[3]} | #{board[4]} | #{board[5]} "
    puts "-----------"
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
- end
+end
+
+def valid_move?(board, index)
+  if !index.between?(0, 8)
+    return false
+  end
+
+  value = board[index]
+  if(value == nil){
+    value = ""
+  }
+
+  return value.trim == ""
+end
